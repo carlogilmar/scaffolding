aaf17ec4eba5b498aa7cbde2cfe54e250852b7a1
Carlo Gilmar <carlogilmar12@gmail.com>
2019-10-14 14:51:11 -0500

    print debuggers

--- lib/mix/task/scaffolding.ex
+++ lib/mix/task/scaffolding.ex
@@ -7,5 +7,13 @@ defmodule Mix.Tasks.ScaffoldView do
     IO.inspect res
     IO.puts "Trying to get my mix confg"
     IO.inspect Mix.Project.config[:app]
+    host_app = Mix.Project.config[:app]
+	  IO.puts "creating a dummy folder in your path"
+    _ = System.cmd("mkdir", ["lib/dummy_folder"])
+		IO.puts "Getting the priv dir from scaffolding"
+    IO.inspect :code.priv_dir(:scaffolding)
+		IO.puts "getting priv dir from host app"
+		IO.inspect :code.priv_dir(host_app)
+		IO.puts "--- Done!!"
   end
 end
